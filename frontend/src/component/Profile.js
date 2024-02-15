351c46f04daa5ecbe3f4ae23aec9c5987fbc8625
Kalpesh Senva <124630059+kalpesh281@users.noreply.github.com>
2024-02-15 16:59:09 +0530

    commit

---
+++ frontend/src/component/Profile.js
@@ -0,0 +1,228 @@
+import { useContext, useEffect, useState } from "react";
+import {
+  Button,
+  Grid,
+  Typography,
+  Modal,
+  Paper,
+  makeStyles,
+  TextField,
+  Avatar,
+} from "@material-ui/core";
+import axios from "axios";
+import ChipInput from "material-ui-chip-input";
+import FileUploadInput from "../lib/FileUploadInput";
+import DescriptionIcon from "@material-ui/icons/Description";
+import FaceIcon from "@material-ui/icons/Face";
+
+import { SetPopupContext } from "../App";
+
+import apiList, {server} from "../lib/apiList";
+
+const useStyles = makeStyles((theme) => ({
+  body: {
+    height: "inherit",
+  },
+  popupDialog: {
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    // padding: "30px",
+  },
+  avatar: {
+    alignSelf:"center",
+    width: theme.spacing(17),
+    height: theme.spacing(17),
+  },
+}));
+
+const MultifieldInput = (props) => {
+  const classes = useStyles();
+  const { education, setEducation } = props;
+
+  return (
+    <>
+      {education.map((obj, key) => (
+        <Grid item container className={classes.inputBox} key={key}>
+          <Grid item xs={6}>
+            <TextField
+              label={`Institution Name #${key + 1}`}
+              value={education[key].institutionName}
+              onChange={(event) => {
+                const newEdu = [...education];
+                newEdu[key].institutionName = event.target.value;
+                setEducation(newEdu);
+              }}
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              label="Start Year"
+              value={obj.startYear}
+              variant="outlined"
+              type="number"
+              onChange={(event) => {
+                const newEdu = [...education];
+                newEdu[key].startYear = event.target.value;
+                setEducation(newEdu);
+              }}
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              label="End Year"
+              value={obj.endYear}
+              variant="outlined"
+              type="number"
+              onChange={(event) => {
+                const newEdu = [...education];
+                newEdu[key].endYear = event.target.value;
+                setEducation(newEdu);
+              }}
+            />
+          </Grid>
+        </Grid>
+      ))}
+      <Grid item style={{ alignSelf: "center" }}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() =>
+            setEducation([
+              ...education,
+              {
+                institutionName: "",
+                startYear: "",
+                endYear: "",
+              },
+            ])
+          }
+          className={classes.inputBox}
+        >
+          Add another institution details
+        </Button>
+      </Grid>
+    </>
+  );
+};
+
+const Profile = (props) => {
+  const classes = useStyles();
+  const setPopup = useContext(SetPopupContext);
+  const [userData, setUserData] = useState();
+  const [open, setOpen] = useState(false);
+
+  const [profileDetails, setProfileDetails] = useState({
+    name: "",
+    education: [],
+    skills: [],
+    resume: "",
+    profile: "",
+  });
+
+  const [education, setEducation] = useState([
+    {
+      institutionName: "",
+      startYear: "",
+      endYear: "",
+    },
+  ]);
+
+  const handleInput = (key, value) => {
+    setProfileDetails({
+      ...profileDetails,
+      [key]: value,
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(apiList.user, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setProfileDetails(response.data);
+        if (response.data.education.length > 0) {
+          setEducation(
+            response.data.education.map((edu) => ({
+              institutionName: edu.institutionName ? edu.institutionName : "",
+              startYear: edu.startYear ? edu.startYear : "",
+              endYear: edu.endYear ? edu.endYear : "",
+            }))
+          );
+        }
+      })
+      .catch((err) => {
+        //console.log(err.response.data);
+        setPopup({
+          open: true,
+          severity: "error",
+          message: "Error",
+        });
+      });
+  };
+
+   
+
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const editDetails = () => {
+    setOpen(true);
+  };
+
+  const handleUpdate = () => {
+    console.log(education);
+
+    let updatedDetails = {
+      ...profileDetails,
+      education: education
+        .filter((obj) => obj.institutionName.trim() !== "")
+        .map((obj) => {
+          if (obj["endYear"] === "") {
+            delete obj["endYear"];
+          }
+          return obj;
+        }),
+    };
+
+    axios
+      .put(apiList.user, updatedDetails, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        setPopup({
+          open: true,
+          severity: "success",
+          message: response.data.message,
+        });
+        getData();
+      })
+      .catch((err) => {
+        setPopup({
+          open: true,
+          severity: "error",
+          message: err.response.data.message,
+        });
+        console.log(err.response);
+      });
+    setOpen(false);
+  };
+  
+
+
+export default Profile;
